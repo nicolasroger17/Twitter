626a7b9b89079393a1c0fc81ff1b1265660c906f
nicolasroger17 <nicolas.roger@isep.fr>
2014-03-26 21:29:12 +0100

    modify php

--- pull/index.php
+++ pull/index.php
@@ -1,6 +1,9 @@
+<h1 style='text-align:center'>Git pull</h1>
+<div>
 <?php
-	echo "pull and push";
 	$ex = shell_exec('sh pull.sh');
 	echo "git message : ";
 	print($ex);
 ?>
+</div>
+<p>Good job nicolasroger17</p>
