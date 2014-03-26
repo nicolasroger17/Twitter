e1c1af94e7896dc85edbb2a430f6de1e8a176745
nicolasroger17 <nicolas.roger@isep.fr>
2014-03-26 21:17:25 +0100

    modify .sh

--- pull/pull.sh
+++ pull/pull.sh
@@ -1,8 +1,4 @@
-#!/bin/sh
-
-touch "lala.test"
-export HOME=/home/pi
-git --git-dir=/var/www/twitter/.git --work-tree=/var/www/twitter/ pull
-git --git-dir=/var/www/twitter/.git --work-tree=/var/www/twitter/ add -A
-git --git-dir=/var/www/twitter/.git --work-tree=/var/www/twitter/ commit -m "commit from server"
-git --git-dir=/var/www/twitter/.git --work-tree=/var/www/twitter/ push
+git pull
+git add -A
+git commit -m "commit from server"
+git push
