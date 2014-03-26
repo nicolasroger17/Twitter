73c8ad6f502c269e91d873be32f1d3fcf6a0195e
nicolasroger17 <nicolas.roger@isep.fr>
2014-03-27 00:31:23 +0100

    test

--- pull/pull.sh
+++ pull/pull.sh
@@ -2,6 +2,6 @@
 
 export HOME=/home/pi
 git --git-dir=/var/www/twitter/.git --work-tree=/var/www/twitter/ pull
-git --git-dir=/var/www/twitter/.git --work-tree=/var/www/twitter/ add -A
-git --git-dir=/var/www/twitter/.git --work-tree=/var/www/twitter/ commit -m "commit from server"
-git --git-dir=/var/www/twitter/.git --work-tree=/var/www/twitter/ push
+#git --git-dir=/var/www/twitter/.git --work-tree=/var/www/twitter/ add -A
+#git --git-dir=/var/www/twitter/.git --work-tree=/var/www/twitter/ commit -m "commit from server"
+#git --git-dir=/var/www/twitter/.git --work-tree=/var/www/twitter/ push
