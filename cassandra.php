5a25f2c56b30ffc1ba337b9a66014fa3b2002578
nicolasroger17 <nicolas.roger@isep.fr>
2014-04-04 14:18:25 +0200

    fix twitter

--- cassandra.php
+++ cassandra.php
@@ -2,11 +2,11 @@
 class Cassandra{
 
 	function __construct(){
-		require_once(__DIR__.'/../lib/autoload.php');
-		use phpcassa\Connection\ConnectionPool;
-		use phpcassa\ColumnFamily;
-		use phpcassa\SystemManager;
-		use phpcassa\Schema\StrategyClass;
+		require_once('phpcassa/lib/autoload.php');
+		use phpcass\lib\phpcassa\Connection\ConnectionPool;
+		use phpcass\lib\phpcassa\ColumnFamily;
+		use phpcass\lib\phpcassa\SystemManager;
+		use phpcass\lib\phpcassa\Schema\StrategyClass;
 
 		// Create a new keyspace and column family
 		$sys = new SystemManager('89.156.6.211:9160');
