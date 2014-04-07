f3c3bd32f84fd3f2e1913109c080ee4d0fef66ed
nicolasroger17 <nicolas.roger@isep.fr>
2014-04-07 13:38:57 +0200

    new base

--- class/manageReturnContent.php
+++ class/manageReturnContent.php
@@ -8,47 +8,12 @@ class ManageReturnContent{
 	}
 
 	function start(){
-  		//var_dump($this->content);
-  		var_dump($this->content['description']);
+  		var_dump($this->content);
+        echo "<br>";
+        echo "<br>";
+  		print($this->content['user']['description']);
  		exit();
 	}
 
-	/*function text_to_array($str) {
-
-        //Initialize arrays
-        $keys = array();
-        $values = array();
-        $output = array();
-
-        //Is it an array?
-        if( substr($str, 0, 5) == 'Array' ) {
-
-            //Let's parse it (hopefully it won't clash)
-            $array_contents = substr($str, 7, -2);
-            $array_contents = str_replace(array('[', ']', '=>'), array('#!#', '#?#', ''), $array_contents);
-            $array_fields = explode("#!#", $array_contents);
-
-            //For each array-field, we need to explode on the delimiters I've set and make it look funny.
-            for($i = 0; $i < count($array_fields); $i++ ) {
-
-                //First run is glitched, so let's pass on that one.
-                if( $i != 0 ) {
-
-                    $bits = explode('#?#', $array_fields[$i]);
-                    if( $bits[0] != '' ) $output[$bits[0]] = $bits[1];
-
-                }
-            }
-
-            //Return the output.
-            return $output;
-
-        } else {
-
-            //Duh, not an array.
-            echo 'The given parameter is not an array.';
-            return null;
-        }
-    }*/
 }
 ?>
